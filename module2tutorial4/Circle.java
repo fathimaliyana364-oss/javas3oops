4faf549072203d6056267abd3aa3c481176d5001
fathimaliyana364-oss <fathimaliyana364@gmail.com>
2026-09-11 20:28:12 +0530

    Create Circle.java

---
+++ module2tutorial4/Circle.java
@@ -0,0 +1,33 @@
+package module2;
+import java.util.Scanner;
+
+class Circle
+{
+    double radius;
+
+    Circle(double r)
+    {
+        radius = r;
+    }
+
+    void display()
+    {
+        double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+
+        System.out.println("Area = " + area);
+        System.out.println("Circumference = " + circumference);
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();
+
+        Circle c = new Circle(r);
+
+        c.display();
+    }
+}
