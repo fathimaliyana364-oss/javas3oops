beddbe47c077c9ac4f285acf09bfdc0ed389625b
fathimaliyana364-oss <fathimaliyana364@gmail.com>
2026-09-12 12:11:02 +0530

    Create VectorSum.java

---
+++ module2tutorial5/VectorSum.java
@@ -0,0 +1,25 @@
+package module2;
+import java.util.Vector;
+
+class VectorSum
+{
+    public static void main(String args[])
+    {
+        Vector<Integer> numbers = new Vector<Integer>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        int sum = 0;
+
+        for(int number : numbers)
+        {
+            sum = sum + number;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
